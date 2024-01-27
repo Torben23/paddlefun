0428b761a003b088949055b703eb13c7c848e60b
Torben <105077862+Torben23@users.noreply.github.com>
2024-01-27 11:24:32 -0600

    up

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,49 +5,46 @@
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach(e => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener('scroll', listener);
+  };
 
   /**
    * Back to top button
    */
-  let backtotop = select('.back-to-top')
+  let backtotop = select('.back-to-top');
   if (backtotop) {
     const toggleBacktotop = () => {
-      if (window.scrollY > 100) {
-        backtotop.classList.add('active')
-      } else {
-        backtotop.classList.remove('active')
-      }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
+      backtotop.classList.toggle('active', window.scrollY > 100);
+    };
+
+    window.addEventListener('load', toggleBacktotop);
+    onscroll(document, toggleBacktotop);
   }
 
   /**
@@ -61,11 +58,10 @@
       mirror: false
     });
   }
-  window.addEventListener('load', () => {
-    aos_init();
-  });
 
-     // Calendario Verga
+  window.addEventListener('load', aos_init);
+
+  // Calendario
   $(document).ready(function() {
     // Configurar el selector de fecha
     $("#datepicker").datepicker({
@@ -76,32 +72,30 @@
       }
     });
   });
-  
-      // Script para enviar mensaje por WhatsApp
-      function sendWhatsAppMessage() {
-        var name = document.getElementById('name').value;
-        var date = document.getElementById('date').value;
-        var people = document.getElementById('people').value;
-        var level = document.getElementById('level').value;
-  
-        // Construct the WhatsApp message with reservation information
-        var whatsappMessage = "Reservation%0A%0AName: " + name + "%0ADate: " + date + "%0APeople: " + people + "%0ALevel: " + level;
-  
-        // Create the WhatsApp link
-        var whatsappLink = "https://wa.me/542216062309/?text=" + whatsappMessage;
-  
-        // Open the link in a new window/tab
-        window.open(whatsappLink, "_blank");
-      }
-  
-      // Add click event listener to the WhatsApp button
-      document.getElementById('whatsappLink').addEventListener('click', function (event) {
-        // Prevenir la acción por defecto del botón (enviar el formulario)
-        event.preventDefault();
-        // Llamar a la función para enviar el mensaje por WhatsApp
-        sendWhatsAppMessage();
-      });
-    });
 
+  // Script para enviar mensaje por WhatsApp
+  function sendWhatsAppMessage() {
+    var name = document.getElementById('name').value;
+    var date = document.getElementById('date').value;
+    var people = document.getElementById('people').value;
+    var level = document.getElementById('level').value;
 
-})()
+    // Construct the WhatsApp message with reservation information
+    var whatsappMessage = "Reservation%0A%0AName: " + name + "%0ADate: " + date + "%0APeople: " + people + "%0ALevel: " + level;
+
+    // Create the WhatsApp link
+    var whatsappLink = "https://wa.me/542216062309/?text=" + encodeURIComponent(whatsappMessage);
+
+    // Open the link in a new window/tab
+    window.open(whatsappLink, "_blank");
+  }
+
+  // Add click event listener to the WhatsApp button
+  on('click', '#whatsappLink', function(event) {
+    // Prevenir la acción por defecto del botón (enviar el formulario)
+    event.preventDefault();
+    // Llamar a la función para enviar el mensaje por WhatsApp
+    sendWhatsAppMessage();
+  });
+
+})();
